866240e46d0195a5b17730f267b4c92698754508
bingdang <431965560@qq.com>
2026-02-03 16:42:25 +0800

    Site updated: 2026-02-03 16:42:14

---
+++ status/config.js
@@ -0,0 +1,31 @@
+// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: 'Site Probe｜Maxbit',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  ApiKeys: [
+    'ur1391520-9ea5c796938e4504513fefba',
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: true,
+
+  // 日志天数
+  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
+  // 不过时间不要设置太长，容易卡，接口请求也容易失败
+  CountDays: 30,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Link',
+      url: '/link/'
+    }
+  ]
+};
